41d723d56f6a67594090d7424adbf1e623ca711c
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2024-10-07 13:43:46 +0530

    Write basic implementation for dropdown behaviour

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
 import "./styles.css";
 
+
+const dropdownMenu = () => {
+  const menuContainer = document.querySelector('.dropdown-menu')
+  const menuContent = menuContainer.querySelectorAll(".menu-item");
+  const menuCaption = document.createElement('div')
+  menuCaption.textContent = menuContainer.id
+  menuCaption.classList.add("menu-line")
+  menuContainer.prepend(menuCaption)
+
+  menuContent.forEach((element) => {
+    element.classList.add("menu-line");
+    element.classList.add("hidden");
+
+  });
+
+  // Controlling how the overall menu works with the mouse
+  menuContainer.addEventListener('mouseover', () => {
+    menuContent.forEach(element => {
+      element.classList.remove('hidden')
+
+    });
+  })
+  menuContainer.addEventListener('mouseout', () => {
+    menuContent.forEach(element => {
+      element.classList.add('hidden')
+    });
+  })
+
+  //Controlling how each menu item works with the mouse
+  menuContent.forEach(item => {
+    //hovering over the item
+    item.addEventListener('mouseover', () => {
+      item.classList.add('hovering-item')
+    })
+
+    item.addEventListener('mouseout', () => {
+      item.classList.remove('hovering-item')
+    })
+
+    //selecting the item while hovering 
+    item.addEventListener('click', () => {
+      menuCaption.textContent = item.textContent
+    })
+  })
+} 
+dropdownMenu()
